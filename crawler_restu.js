a1362d16692fab13acd9688a5dc5e25b6f91fa5b
marketaspinkova <41306234+marketaspinkova@users.noreply.github.com>
2018-11-29 16:15:10 +0100

    Add files via upload

---
+++ crawler_restu.js
@@ -0,0 +1,41 @@
+function pageFunction(context) {
+
+	var $ = context.jQuery;
+    
+    if ((! window.location.href.endsWith('/hodnoceni/#tabs')) && (! window.location.href.endsWith('#restaurant-review-list-anchor')))
+        return;
+    var overallData = {
+        restaurantName: $(".restaurant-detail-heading-container h2").text(),
+        review: $('figure.rating-chart figcaption').text(),
+        address: $("address#restaurant-address").text(),
+        features: $("ul.restaurant-desc-list a").text()
+    };
+    
+    var reviews = []
+    
+    $('article.restaurant-review-item').each(function() {
+        
+            var food =  $(this).find(".review-item__rating-star-number:eq(0)").text();
+            var staff = $(this).find(".review-item__rating-star-number:eq(1)").text();
+            var env =   $(this).find(".review-item__rating-star-number:eq(2)").text();
+        
+        reviews.push({
+            rev_url: $(this).find("a.restaurant-review-item-link").attr("href"),
+            username: $(this).find("li.review-item__guest").text(),
+            date: $(this).find("li.review-item__reservation span").attr("content"),
+            text: $(this).find("p.restaurant-review-item__summary:eq(0)").text(),
+            foodRating: food,
+            staffRating: staff,
+            environmentRating: env,
+            totalRating: (parseInt(food, 10) + parseInt(staff, 10) + parseInt(env, 10)) / 3
+        })
+        
+    });
+    
+    var result = {
+        restaurantData: overallData,
+        individualReviews: reviews
+    }
+    
+    return result;
+}
