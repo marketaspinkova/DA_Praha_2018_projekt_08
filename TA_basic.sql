33c185bb5958168b2e50be74acd2f0129eecbb90
marketaspinkova <41306234+marketaspinkova@users.noreply.github.com>
2018-11-30 14:33:05 +0100

    Update TA_basic.sql

--- TA_basic.sql
+++ TA_basic.sql
@@ -1,6 +1,9 @@
 -- TA_MIX_PRAHA table____________________________________________________
 
 -- deletes error lines and errorInfo column
+/* While scraping data, several times errors occured and data for a certain review were not downloaded. 
+In this case column errorInfo. Solution: first remove rows where errorInfo is not empty, then remove column errorInfo */
+
 delete from TA_bakeries_praha
 where "errorInfo" != ''
 ;
@@ -45,3 +48,22 @@ alter table ta_rest_praha drop column "errorInfo";
 --- deletes empty ID (rev_url) - no reviews for these places yet
 delete from ta_rest_praha
 where "rev_url" = '';
+
+create table ta_rest_praha_cleaned as
+select ta.*
+from ta_rest_praha ta;
+
+-- from Geneea app in Keboola I got files with language detection - analysis-result-documents
+
+create table ta_mix_praha_lang as
+select ta.*, an."language"
+from ta_mix_praha ta
+join "analysis-result-documents" an
+on ta."rev_url" = an."rev_url";
+
+create table TA_rest_praha_lang as
+select c.*,
+an."language"
+from ta_rest_praha_cleaned c
+join "analysis-result-documents" an
+on c."rev_url" = an."rev_url";
